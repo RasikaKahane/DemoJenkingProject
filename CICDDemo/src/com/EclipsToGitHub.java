0363a0e8eaff24dbd232b90a8239b8c5aa1b6506
Rasika <rasikashedage@yahoo.com>
2026-04-21 17:34:47 +0530

    3rd exp

--- CICDDemo/src/com/EclipsToGitHub.java
+++ CICDDemo/src/com/EclipsToGitHub.java
@@ -4,9 +4,9 @@ public class EclipsToGitHub {
 
 	public static void main(String[] args) {
 
-
+		EclipsToGitHub El=new EclipsToGitHub();
 		System.out.println("Hello Rasika");
-
+		
 	}
 	public void m1()
 	{
